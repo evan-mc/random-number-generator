9a98fbaf876e23cd0a9f51b1090b8102add3ad9a
Evan Carter <emcarter@uci.edu>
2020-08-09 23:36:14 -0700

    added min and max number option for user to use instead of digit length

--- app/src/main/java/android/example/firstapp/MainActivity.kt
+++ app/src/main/java/android/example/firstapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package android.example.firstapp
 
 import android.app.Activity
+import android.content.Context
 import android.example.firstapp.databinding.ActivityMainBinding
+import android.opengl.Visibility
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -12,8 +14,8 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatCheckBox
 import kotlin.random.Random
+import kotlin.random.nextInt
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,7 +32,8 @@ class MainActivity : AppCompatActivity() {
         var countdown : Int = 0
         override fun run() {
             Log.i("MainActivity", "called repeatRollUntilStopped.run()")
-            binding.numberTextView.text = randomNum(binding.numberLength.text)
+
+            chooseRandomNum()
 
             handler.postDelayed(this, 100)
 
@@ -50,8 +53,19 @@ class MainActivity : AppCompatActivity() {
                 binding.timerEditText.visibility = View.VISIBLE
                 binding.timerLengthTextView.visibility = View.VISIBLE
 
-                binding.numberLength.visibility = View.VISIBLE
-                binding.digitTextView.visibility = View.VISIBLE
+                //hide timer text view until start button pressed
+                binding.timerTextView.visibility = View.INVISIBLE
+
+                if(binding.rangeCheckbox.isChecked)
+                {
+                    binding.minText.visibility = View.VISIBLE
+                    binding.maxText.visibility = View.VISIBLE
+                }
+                else
+                {
+                    binding.numberLength.visibility = View.VISIBLE
+                    binding.digitTextView.visibility = View.VISIBLE
+                }
             }
 
             countdown += 100
@@ -76,7 +90,7 @@ class MainActivity : AppCompatActivity() {
         binding.timerTextView.text = getString(R.string.timer_text, time.toString())
 
         binding.rollButton.setOnClickListener {
-            binding.numberTextView.text = randomNum(binding.numberLength.text)
+            chooseRandomNum()
         }
 
         binding.startButton.setOnClickListener {
@@ -87,9 +101,13 @@ class MainActivity : AppCompatActivity() {
                 //hide ui elements that shouldnt be visible during the timer
                 binding.timerEditText.visibility = View.GONE
                 binding.timerLengthTextView.visibility = View.GONE
-
                 binding.numberLength.visibility = View.GONE
                 binding.digitTextView.visibility = View.GONE
+                binding.minText.visibility = View.GONE
+                binding.maxText.visibility = View.GONE
+
+                //display timer countdown
+                binding.timerTextView.visibility = View.VISIBLE
             }
         }
 
@@ -107,8 +125,19 @@ class MainActivity : AppCompatActivity() {
             binding.timerEditText.visibility = View.VISIBLE
             binding.timerLengthTextView.visibility = View.VISIBLE
 
-            binding.numberLength.visibility = View.VISIBLE
-            binding.digitTextView.visibility = View.VISIBLE
+            if(binding.rangeCheckbox.isChecked)
+            {
+                binding.minText.visibility = View.VISIBLE
+                binding.maxText.visibility = View.VISIBLE
+            }
+            else
+            {
+                binding.numberLength.visibility = View.VISIBLE
+                binding.digitTextView.visibility = View.VISIBLE
+            }
+
+            //display countdown timer
+            binding.timerTextView.visibility = View.INVISIBLE
         }
 
         binding.numberLength.setOnKeyListener { _, keyCode, _ ->
@@ -120,11 +149,12 @@ class MainActivity : AppCompatActivity() {
                 //set cursor to end of edittext
                 binding.numberLength.setSelection(binding.numberLength.text.length)
             }
-            else if(binding.numberLength.text[0] == '0') //strip leading 0 if adding another digit
+            else if(binding.numberLength.text[0] == '0') { //strip leading 0 if adding another digit
                 when (keyCode) {
                     in 8..16 -> binding.numberLength.setText(binding.numberLength.text.substring(1))
                     //8 through 16 is the keycode event for numbers 1 through 9
                 }
+            }
             false
         }
 
@@ -145,17 +175,56 @@ class MainActivity : AppCompatActivity() {
 
                 hideKeyboard(this)
             }
-            else if(binding.timerEditText.text[0] == '0') //strip leading 0 if adding another digit
+            else if(binding.timerEditText.text[0] == '0') {//strip leading 0 if adding another digit
                 when (keyCode) {
                     in 8..16 -> binding.timerEditText.setText(binding.timerEditText.text.substring(1))
                     //8 through 16 is the keycode event for numbers 1 through 9
                 }
+            }
+            false
+        }
+
+        binding.minNumRoll.setOnKeyListener { _, keyCode, _ ->
+
+            //prevents edittext from being empty
+            if(keyCode == KeyEvent.KEYCODE_DEL && binding.minNumRoll.text.isEmpty()) {
+
+                binding.minNumRoll.setText("0")
+
+                //set cursor to end of edittext
+                binding.minNumRoll.setSelection(binding.minNumRoll.text.length)
+            }
+            else if(binding.minNumRoll.text[0] == '0') { //strip leading 0 if adding another digit
+                when (keyCode) {
+                    in 8..16 -> binding.minNumRoll.setText(binding.minNumRoll.text.substring(1))
+                    //8 through 16 is the keycode event for numbers 1 through 9
+                }
+            }
+            false
+        }
+
+        binding.maxNumRoll.setOnKeyListener { _, keyCode, _ ->
+
+            //prevents edittext from being empty
+            if(keyCode == KeyEvent.KEYCODE_DEL && binding.maxNumRoll.text.isEmpty()) {
+
+                binding.maxNumRoll.setText("0")
+
+                //set cursor to end of edittext
+                binding.maxNumRoll.setSelection(binding.maxNumRoll.text.length)
+            }
+            else if(binding.maxNumRoll.text[0] == '0') { //strip leading 0 if adding another digit
+                when (keyCode) {
+                    in 8..16 -> binding.maxNumRoll.setText(binding.maxNumRoll.text.substring(1))
+                    //8 through 16 is the keycode event for numbers 1 through 9
+                }
+            }
             false
         }
 
     }
 
-    private fun randomNum(digitLength: Editable): String {
+    private fun randomDigitNum(digitLength: Editable): String {
 
         val length : Int = digitLength.toString().toInt()
         val retNum : StringBuilder = StringBuilder(length)
@@ -167,47 +236,108 @@ class MainActivity : AppCompatActivity() {
        return retNum.toString()
     }
 
+    private fun randomRangeNum(minNum: Editable, maxNum: Editable) : String {
+        val min : Int = minNum.toString().toInt()
+        val max : Int = maxNum.toString().toInt()
+
+        return if(min <= max) {
+            Random.nextInt(min..max).toString()
+        } else {
+            binding.numberTextView.text.toString()
+        }
+    }
+
+    private fun chooseRandomNum() {
+        if(binding.rangeCheckbox.isChecked) {
+            binding.numberTextView.text = randomRangeNum(binding.minNumRoll.text, binding.maxNumRoll.text)
+        }
+        else {
+            binding.numberTextView.text = randomDigitNum(binding.numberLength.text)
+        }
+    }
+
     fun onCheckboxClicked(view: View) {
         if(view is CheckBox) {
 
-            if(binding.checkbox.isChecked) {
-                //hide roll button, show stop, start and timer countdown
-                binding.rollButton.visibility = View.GONE
+            val checked : Boolean = view.isChecked
 
-                binding.startButton.visibility = View.VISIBLE
-                binding.stopButton.visibility = View.VISIBLE
-
-                binding.timerTextView.visibility = View.VISIBLE
-
-                binding.timerEditText.visibility = View.VISIBLE
-                binding.timerLengthTextView.visibility = View.VISIBLE
+            when(view.id) {
+                binding.timerCheckbox.id -> handleTimerCheckbox(checked)
+                binding.rangeCheckbox.id -> handleRangeCheckbox(checked)
             }
-            else {
-                //do the opposite
-                repeatRollUntilStopped.countdown = 0
-                handler.removeMessages(0)
-                hasStartedTimer = false
+        }
+    }
 
-                binding.rollButton.visibility = View.VISIBLE
+    private fun handleTimerCheckbox(checked : Boolean) {
+        if(checked) {
+            //hide roll button, show stop, start and timer countdown
+            binding.rollButton.visibility = View.GONE
 
-                binding.timerTextView.text = getString(R.string.timer_text, time.toString())
+            binding.startButton.visibility = View.VISIBLE
+            binding.stopButton.visibility = View.VISIBLE
 
-                binding.startButton.visibility = View.GONE
-                binding.stopButton.visibility = View.GONE
+            binding.timerEditText.visibility = View.VISIBLE
+            binding.timerLengthTextView.visibility = View.VISIBLE
+        }
+        else {
+            //do the opposite
+            repeatRollUntilStopped.countdown = 0
+            handler.removeMessages(0)
+            hasStartedTimer = false
 
-                binding.timerTextView.visibility = View.GONE
+            binding.rollButton.visibility = View.VISIBLE
 
-                binding.timerEditText.visibility = View.GONE
-                binding.timerLengthTextView.visibility = View.GONE
-            }
+            binding.timerTextView.text = getString(R.string.timer_text, time.toString())
 
+            binding.startButton.visibility = View.GONE
+            binding.stopButton.visibility = View.GONE
+
+            binding.timerTextView.visibility = View.INVISIBLE
+
+            binding.timerEditText.visibility = View.GONE
+            binding.timerLengthTextView.visibility = View.GONE
+        }
+
+        if(!binding.rangeCheckbox.isChecked)
+        {
             //make visible in the event they were hidden from starting the timer
             binding.numberLength.visibility = View.VISIBLE
             binding.digitTextView.visibility = View.VISIBLE
         }
     }
 
-    fun hideKeyboard(activity: Activity) {
+    private fun handleRangeCheckbox(checked : Boolean) {
+        if(checked) {
+            /*
+                1: hide num of digits and its textview
+                2: display min num and max num range edittexts and textviews
+             */
+
+            binding.digitTextView.visibility = View.GONE
+            binding.numberLength.visibility = View.GONE
+
+            binding.minNumRoll.visibility = View.VISIBLE
+            binding.maxNumRoll.visibility = View.VISIBLE
+
+            binding.minText.visibility = View.VISIBLE
+            binding.maxText.visibility = View.VISIBLE
+        }
+        else
+        {
+            //do the opposite
+
+            binding.digitTextView.visibility = View.VISIBLE
+            binding.numberLength.visibility = View.VISIBLE
+
+            binding.minNumRoll.visibility = View.GONE
+            binding.maxNumRoll.visibility = View.GONE
+
+            binding.minText.visibility = View.GONE
+            binding.maxText.visibility = View.GONE
+        }
+    }
+
+    private fun hideKeyboard(activity: Activity) {
         val imm: InputMethodManager =
             activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         //Find the currently focused view, so we can grab the correct window token from it.
